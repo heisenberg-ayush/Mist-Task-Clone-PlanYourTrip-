f8ffc85437f112b4ac09fe3321f8846fef2cd965
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2021-08-15 01:00:47 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Scroll top button
+var myButton = document.getElementById('scrollTopBtn');
+
+// When the User clicks the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
